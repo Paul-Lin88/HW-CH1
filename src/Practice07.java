9af2ff77585a080f6c3dacdd3b6916f16c8b32d0
Paul-Lin88 <t109360116@ntut.org.tw>
2022-10-04 03:40:09 +0800

    first commit

---
+++ src/Practice07.java
@@ -0,0 +1,11 @@
+public class Practice07
+{
+public static void main(String[] args)
+	{
+	int num =3;
+	System.out.println("變數num更新後的的值是:"+num);
+	num = 5;
+	System.out.println("更新變數num的值");
+	System.out.println("變數num更新後的值是:"+num);
+	}
+}
